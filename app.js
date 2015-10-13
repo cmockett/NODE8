f200b59a630db594ec8ff3fe19e351f810f9e937
Student <student@RU-109.local>
2015-10-13 16:10:54 -0600

    2nd

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mongoapp');
+mongoose.connect('mongodb://localhost/mycompanyname');
 // Create Express App Object \\
 var app = express();
 
@@ -19,6 +19,17 @@ app.get('/', function(req, res) {
 });
 
 // displays a list of applicants
+app.get('/getapplicants', function(req, res){
+	NewApplicant.find({}, function(err, data){
+		if(err){
+			console.log("error!!")
+		}
+		else{
+			res.send(data)
+		}
+	})
+})
+
 app.get('/applicants', function(req, res){
 	res.sendFile('html/applicants.html', {root : './public'});
 });
@@ -26,6 +37,12 @@ app.get('/applicants', function(req, res){
 // creates and applicant
 app.post('/applicant', function(req, res){
 	console.log(req.body)
+	var newGuy = new NewApplicant(req.body);
+	newGuy.save(function(err){
+		if(err){
+			console.log('error!');
+		}
+	})
 	// Here is where you need to get the data
 	// from the post body and store it in the database
 	res.redirect('/success');
@@ -34,15 +51,19 @@ app.post('/applicant', function(req, res){
 app.get('/success', function(req, res){
 	res.send("Super Success!!")
 })
+// app.get('/applicants', function(req, res){
+	// res.send(req.body)
 
-var newApplicant = mongoose.model('newApplicant',
+// })
+
+var NewApplicant = mongoose.model('NewApplicant',
 	{ name : String,
-	Bio : String,
-	Skills : String, 
-	YearsExp : Number,
-	WhyWorkHere: String,
+	bio : String,
+	skills : String, 
+	years : Number,
+	why: String,
 	}
-
+)
 // Creating Server and Listening for Connections \\
 var port = 3000
 app.listen(port, function(){
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,6 +6,17 @@ angular.module('JobApp')
 	}]);
 
 angular.module('JobApp')
-	.controller('applicantController', ['$scope', function($scope){
-		
+	.controller('applicantController', ['$scope', '$http', function($scope, $http){
+		$http.get('/getapplicants').then(function(returnData){
+			console.log("Hi!" + returnData)},
+			function(error){
+				console.log('error')
+			}
+		)
+		$http.get('/applicants').then(function(returnData){
+
+			
+		$scope.applicantArray = []
+		$scope.applicantArray.push(returnData)
+		})
 	}]);
